4a3a5cf16591f16e74a43cf0b5bb7a0fb87a62e9
David Lee <dyl30@case.edu>
2023-11-03 22:19:34 -0400

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {15,6,2,12,4};
-        SelectionSort.selectionSort(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
